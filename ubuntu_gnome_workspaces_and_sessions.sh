e12153b0c66112cfdb3e78f5d0775d5c033fe3ec
danilosaft <danilo.saft@googlemail.com>
2020-10-24 22:30:04 +0200

    Create ubuntu_gnome_workspaces_and_sessions.sh
    Adds Workspace Matrices and Window Session Manager

---
+++ ubuntu_gnome_workspaces_and_sessions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt install gnome-tweak-tool
+gnome-tweaks
+
+mkdir ~/.local/share/gnome-shell/extensions/
+cd ~/.local/share/gnome-shell/extensions/
+git clone https://aur.archlinux.org/gnome-shell-extension-workspace-matrix.git
+cd gnome-shell-extension-workspace-matrix
+makepkg -sri
+
+#window session manager extension
+apt install nodejs
+node -v
+npm install -g linux-window-session-manager
+which lwsm
+mkdir ~/.local/share/gnome-shell/extensions/lwsm@johannes.super-productivity.com
+cd tmp
+git clone git@github.com:johannesjo/gnome-shell-extension-window-session-manager.git
+mv gnome-shell-extension-window-session-manager /home/$(whoami)/.local/share/gnome-shell/extensions/lwsm@johannes.super-productivity.com
+
+
